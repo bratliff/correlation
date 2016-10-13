e7022bcfcfed263ef775a369fa9bceb04745c741
Ben Ratliff <bratliff@amplify.com>
2016-10-13 17:44:42 -0400

    filled out design structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 var sass        = require('gulp-sass');
+var reload      = browserSync.reload;
+
+var src = {
+    scss: 'app/scss/*.scss',
+    css:  'app/css',
+    html: 'app/*.html'
+};
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -10,16 +17,17 @@ gulp.task('serve', ['sass'], function() {
         server: "./app"
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// Compile sass into CSS
 gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+    return gulp.src(src.scss)
         .pipe(sass())
-        .pipe(gulp.dest("/css"))
-        .pipe(browserSync.stream());
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
 });
 
 gulp.task('default', ['serve']);
